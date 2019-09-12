b2b2cc523f18ddf2a911ce66e850e0c69d87b1ba
BHARDWAJ Paras Ext OBS/CMI <bnsh4793@orange.com>
2019-09-12 12:07:54 +0530

    new commit

--- src/hello.java
+++ src/hello.java
@@ -1,6 +1,15 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World !!");
+        int fact=1;
+        int n=5;
+        int i;
+        for(i=1;i<=n;i++)
+        {
+            fact*=i;
+
+        }
+        System.out.println(fact);
         System.out.println("Paras");
         System.out.println("Arjun");
 
